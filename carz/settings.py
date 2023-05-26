33b8285f8af2e370c0bfee24fff50a27d1c1a5b1
akaki ivanishvili <akaki@inspectorio.com>
2023-05-26 19:19:47 +0400

    change bucket

--- carz/settings.py
+++ carz/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-GS_BUCKET_NAME = "carz_static"
+GS_BUCKET_NAME = env("GS_BUCKET_NAME")
 GS_LOCATION = "static"
 STATIC_URL = f'https://storage.googleapis.com/'
 STATIC_ROOT = "static/"
